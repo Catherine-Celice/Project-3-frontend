cb57c665e2c3575014c909f4e95b16c07a815a61
Catherine Celice <CCelice@bcbsm.com>
2021-12-02 20:58:48 -0500

    updated User interface to include the extra info and added a Preferences interface to make defining of Users cleaner.

--- project_3/src/models/User.ts
+++ project_3/src/models/User.ts
@@ -1,3 +1,4 @@
+import Preferences from './preferences';
 
 export default interface User {
     _id?: string;
@@ -5,11 +6,12 @@ export default interface User {
     lastname: string;
     email: string;
     phone?: string;
-    userID: string;
-    password1: string;      // CAC: I had to add the 1 to this
-                            // to get rid of an error in addUser() in CreateYourProfile.tsx
-                            
-    petList?: string[];     // this is to keep track of the favorited pets
+    password1: string;
+    image: string;
+    zip: string;
+    aboutme: string;
+    preferences: Preferences;
+    petList?: string[];
 };
 // CAC: In this (frontend) version of the User I declared _id to be a string rather
 // than an ObjectId because that is what Megan did in her firebase lab code.
