bb421f2956c66054ccb6c0411d6cc1136ca7364a
Catherine Celice <CCelice@bcbsm.com>
2021-12-02 19:40:08 -0500

    added petList to user profile interface

--- project_3/src/models/User.ts
+++ project_3/src/models/User.ts
@@ -8,7 +8,8 @@ export default interface User {
     userID: string;
     password1: string;      // CAC: I had to add the 1 to this
                             // to get rid of an error in addUser() in CreateYourProfile.tsx
-
+                            
+    petList?: string[];     // this is to keep track of the favorited pets
 };
 // CAC: In this (frontend) version of the User I declared _id to be a string rather
 // than an ObjectId because that is what Megan did in her firebase lab code.
