2ed4fd9e648223f263e577c2b3b5864bb392831b
Heidi Youmans <heidi.youmans@stellantis.com>
2021-12-05 21:48:58 -0500

    backup changes

--- project_3/src/App.tsx
+++ project_3/src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import PetList from './components/PetList';
-
 import UsersRoute from './routes/UsersRoute';
+
+
+// https://blog.logrocket.com/react-router-v6/
 
 function App() {
   return (
     <div className="App"> 
-    <UsersRoute/>     
-    <PetList/>
+        <Router>
+          <Routes>
+            <Route path="/" element={<UsersRoute/> } />
+            <Route path="/UsersRoute" element={<UsersRoute/> } />
+            <Route path="/PetList" element={<PetList/> } />
+          </Routes>
+        </Router>
     </div>
   );
 }
