b772f368e0a6ffef792995e008e4a285a67ea8ab
Salena Galloway <sgalloway37@gmail.com>
2021-12-06 21:59:52 -0500

    PetFinder Card Swiping
    PetFinder Card Swiping

--- project_3/src/services/PetService.ts
+++ project_3/src/services/PetService.ts
@@ -2,7 +2,7 @@ import Pet from '../models/pet'
 import axios from "axios";
 
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJoaWhTT1oyNERKYmN5VGJqUXdJQUJ4akhCU1gzVHBVdW56VjNOaWRhdENrbkNva2Q4dCIsImp0aSI6IjFlNmZhZDI2ODUzOTAzZjdmNmYwZTIyNGY2NDZiNTcyNTc4OWFhN2M3M2RlZmE0YTQxYTYxZmRkNmRkODQwMDI4YzNjY2YyNWNmMzQwODgyIiwiaWF0IjoxNjM4ODI0NTE1LCJuYmYiOjE2Mzg4MjQ1MTUsImV4cCI6MTYzODgyODExNSwic3ViIjoiIiwic2NvcGVzIjpbXX0.LgqJuPcY5ehBi2j-uoU2u6Oc34WGQoMVPqCODNOIBYrCevC2YSP9_REH8M0Hk5aY2nGtF6rj17AYtny2DM-1SaWfaOdVL8ybv8XilfK-ALxUr2bGuD6oWVd486qgQyJ5yRPd8_nB74y5Sec5i9peVJfa-4vZnDFWn7xoYabaBnQhkOdoMuCBCPiFPDQ2NqCLa2zmHzD2hhTZ_DgEtgSa6hVUZxwOTYbyBmy2zJSWyFMOO5GBxj3v1UfBNecFrTc2cu3A23EqoOhlcUysrID5n36QuR-Boi0uLFcl01hXzsv7AtaLKnwW6FSvrzCS9bbIQDL-LtXD-f5gl4LWR8-vCw'
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJoaWhTT1oyNERKYmN5VGJqUXdJQUJ4akhCU1gzVHBVdW56VjNOaWRhdENrbkNva2Q4dCIsImp0aSI6ImY4NmJjOGI0MmUxODVjMWIxNGEzMjg4MWQ3YWM3NjJkYjc5ZDEzZDUyMzIxOTE3OTRjYzFiMTNiNDY1ZjI1NTVlMWM5OTI4MzI3ZmM5Y2M2IiwiaWF0IjoxNjM4ODQzNDk4LCJuYmYiOjE2Mzg4NDM0OTgsImV4cCI6MTYzODg0NzA5OCwic3ViIjoiIiwic2NvcGVzIjpbXX0.Fu2C0r3fkSPDOT4NaNtBd03ceKPb9ehVpdyoc4_gM_XOUNHoDi3rXpVNXbOSlZ9clqwlEllZb2Rw845sP6B087vAlcfhapiSzbgC6HrnhpVLeOAymBWyRK9vw3Z2_ydK1CyXozuTOst5qbBBOeYRbUfiDgcnN5aWAbT9dj25VsG9vOVoUuBo3OHZFqxp4rtbH7dJMQZmgfAhYSVN6-pRbcvDhXod47gKOU8RIjQaCNsj9A5zdOGuDIwxdosc2SjSbKXwysYMx7nZhedK4pTElT6mafQPH3Y9tVYWxcWNrRpADrmflBBHtLQMnDvm8cnPcDKIFaUl3TKz-AnlXLcQVQ'
 const baseUrl = 'https://api.petfinder.com/v2/animals';
 
  const config = {
@@ -22,4 +22,38 @@ export function getPetDetail(petId: string):Promise<Pet> {
     return axios.get(`${baseUrl}/${petId}`,config).then((response) => {
     return response.data.animal;
     });
+}
+    export function searchAvailablePets(type:string, age:string, gender:string, size:string, goodWithChildren:string, goodWithDogs:string, goodWithCats:string, location:string):Promise<Pet[]> {
+        // https://api.petfinder.com/v2/animals?type=dog&size=Medium&gender=Male
+        let query = '';
+        if(type){
+            query.concat(`?type=${type}`)
+        }
+        if(gender){
+            query.concat(`?gender=${gender}`)
+        }
+        if(age){
+            query.concat(`?age=${age}`)
+        }
+        if(size){
+            query.concat(`?size=${size}`)
+        }
+        if(goodWithChildren){
+            query.concat(`?good_with_children=${goodWithChildren}`)
+        }
+        if(goodWithDogs){
+            query.concat(`?good_with_dogs=${goodWithDogs}`)
+        }
+        if(goodWithCats){
+            query.concat(`?good_with_cats=${goodWithCats}`)
+        }
+        if(location){
+            query.concat(`?location=${location}`)
+        }
+        
+        return axios.get(`${baseUrl}/${query}`,config).then((response) => {
+        return response.data.animals;
+        });
+
+
 }
