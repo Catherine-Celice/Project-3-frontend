b1839041910bcdd1d95f2d21be9f561e06e0f20c
Hannah <hannahsvendor@gmail.com>
2021-12-13 16:23:39 -0500

    CSS changes to navigation
    Made responsive changes to the navigation

--- project_3/src/components/DesktopNav.tsx
+++ project_3/src/components/DesktopNav.tsx
@@ -10,7 +10,7 @@ function DesktopNav(){
                 <div className="NavBar">
                     <Link to="/"><h1 className="NavPP"><img className="NavIcon" src="/images/Avatars/pawfectpals.png" width="60"/>PawfectPals</h1></Link>
                 <div className="NavBarLinks">
-                    <a><Link to="/viewpets" id="NavPets">View Available Pets</Link></a>
+                    <a><Link to="/viewpets" id="NavPets">Available Pets</Link></a>
                     <a><Link to="/myprofile" id="NavProfile">My Profile</Link></a>
                     <button className="navSignUp"><Link to="/signup" className="buttons">Sign Up</Link></button>
                 </div>
