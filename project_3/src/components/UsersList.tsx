92ccb64c28a48b4a4ade72d503b46aafba204579
Heidi Youmans <heidi.youmans@stellantis.com>
2021-12-15 18:41:46 -0500

    backup changes

--- project_3/src/components/UsersList.tsx
+++ project_3/src/components/UsersList.tsx
@@ -1,17 +1,17 @@
 //This is really the user profile, it was refurbished for this purposed.
 //It was originally used to get the api working.
 
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import User from "../models/User";
 import '../styles/UserList.css';
 import DesktopNav from "./DesktopNav";
 
-//interface Props {
-//  user: User;
-//}
+interface Props {
+ onSubmit: (user: User) => void;
+}
 
 //function UsersList({user}: Props) {
-  function UsersList() {
+  function UsersList({onSubmit}: Props) {
       
      const [users, setUsers] = useState<User[]>([
     //   {image: "cat1_avatar", firstname: "Hanna", lastname: "Svendor", email: "Hanna123@gmail.com", phone: "123-456-1234", password1: "newpassword"},
@@ -20,50 +20,92 @@ import DesktopNav from "./DesktopNav";
     //   {image: "dog2_avatar", firstname: "Catherine", lastname: "Celice", email: "Catherine123@gmail.com", phone: "875-423-1234", password1: "TooMuchProgramming"},
      ]);
 
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [password1, setPassword1] = useState("");
+    const [zip, setZip] = useState("");
+    const [aboutme, setAboutMe] = useState("");
+
+    function handleSubmit(e: FormEvent) {
+      e.preventDefault();
+      const user: User = {
+        firstname,
+        lastname,
+        email,
+        phone,
+        password1,
+        zip,
+        aboutme,
+        isLoggedIn: false,
+        petList: []
+      };
+      onSubmit(user);
+      setFirstname('');
+      setLastname('');
+      setEmail('');
+      setPhone('');
+      setPassword1('');
+      setZip('');
+      setAboutMe('');
+    }
+    
   return (
     <>
-    <div className="UserProfile">
-      <div className="UserProfile__header">
-        <h2>User Profile</h2>
-      </div>
-      <div className="Profile">
-         <span>
-          <img className="UserList__image" src={`../images/Avatars/${users[0].image}.png`} />
-        </span>
-        <p>
-          <label className="first">First Name: </label>
-          <span className="UserList__first">{users[0].firstname} </span>
-        </p>
-        <p>
-          <label className="last">Last Name: </label>
-          <span className="UserList__last">{users[0].lastname}</span>
-        </p>
-        <p>
-          <label className="email">Email: </label>
-          <span className="UserList__email">{users[0].email}</span>
-        </p>
-        <p>
-          <label className="phone">Phone: </label>
-          <span className="UserList__phone">{users[0].phone}</span>
-        </p>
-        <p>
-          <label className="password">Password: </label>
-          <span className="UserList__password">{users[0].password1}</span>
-        </p>
-        <p>
-          <label className="zip">Zip Code: </label>
-          <span className="UserList__zip">{users[0].zip}</span>
-        </p>
-        <p className="about">
-          <label className="aboutMe">About Me: </label>
-          <span className="UserList__aboutme">{users[0].aboutme}</span>
-        </p>
-        <p>
-          <label className="prefrences">Preferences: </label>
-          <span className="UserList__prefrences">{users[0].preferences}</span>
-        </p>
-      </div>
-    </div>
+      <form className='UserProfile' onSubmit={handleSubmit}>
+        <div className="UserProfile__header">
+          <h2>User Profile</h2>
+        </div>
+        <div className="Profile">
+          <span>
+            <img className="UserList__image" src={`../images/Avatars/${users[0].image}.png`} />
+          </span>
+          <p>
+            <label htmlFor="UserListForm__first">
+              First Name: 
+              <input className="UserList__first" value={firstname} onChange={e => setFirstname(e.target.value)} />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="UserListForm__last">
+              Last Name:
+              <input className="UserList__last" value={lastname} onChange={e => setLastname(e.target.value)} />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="UserListForm__email">
+              Email:
+              <input className="UserList__email" value={email} onChange={e => setEmail(e.target.value)} />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="UserListForm__phone">
+              Phone:
+              <input className="UserList__phone" value={phone} onChange={e => setPhone(e.target.value)} />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="UserListForm__password">
+              Password:
+              <input className="UserList__password" value={password1} onChange={e => setPassword1(e.target.value)} />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="UserListForm__zip">
+              Zip Code:
+              <input className="UserList__zip" value={zip} onChange={e => setZip (e.target.value)} />
+            </label>
+          </p>
+          <p className="about">
+            <label htmlFor="UserListForm__aboutMe">
+              About Me:
+              <input className="UserList__aboutme" value={aboutme} onChange={e => setAboutMe (e.target.value)} />
+            </label>
+          </p>
+        </div>
+        <input className="updateUser" type="submit" value="Submit Profile Update" />
+      </form>
     </>
   );
 }
--- project_3/src/routes/UsersRoute.tsx
+++ project_3/src/routes/UsersRoute.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import User from "../models/User";
-import { fetchUser } from "../services/UserService";
+import { fetchUser, updateUser } from "../services/UserService";
 import UsersList from "../components/UsersList";
 import { useParams } from "react-router";
 import  '../App.css';
@@ -14,23 +14,23 @@ function UsersRoute() {
   //const history = useNavigate();
   const { email } = useParams<string>();
   const { password1, setPassword1 } = useParams<string>();
-  const [ user, setUser ] = useState<User>();  
+  const [user, setUser] = useState<User>();
 
-
-    // useEffect(() => {
-    //   if(email && password1) {
-    //     fetchUser(email, password1).then(res => setUser(res));
-    //   }
-    //   else if(!email || ! password1) {
-    //     alert('No user found. Would you like to create a profile?');
-    //   }
-    // }, [email, password1]);
+  function UpdateUserProfile(user: User): void {
+    setUser(prev => [... prev, user]);
+    updateUser(user);
+  };
+    useEffect(() => {
+        fetchUser(email, password1).then(res => setUser(res));
+      });
   
     return (
       <div className="UserProfileRoute__body">
         <div className="UserProfileRoute">
          
-          <UsersList/>
+          <UsersList onSubmit={function (user: User): void {
+            throw new Error("Function not implemented.");
+          } }/>
           <PreferencesForm/>
           {/* <UsersList user={user!}/> */}
           <div className="UserProfile__button">
