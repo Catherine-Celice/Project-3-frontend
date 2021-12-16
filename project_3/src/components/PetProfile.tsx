506f8fabbd78aa2168383597c716d6125e3e192a
Hannah <hannahsvendor@gmail.com>
2021-12-15 19:01:25 -0500

    PetProfile_BackButton
    Swapped X for back arrow

--- project_3/src/components/PetProfile.tsx
+++ project_3/src/components/PetProfile.tsx
@@ -35,7 +35,7 @@ function PetProfile() {
               <p>{pet.photos.map((pics) => { return <img style={{ width: "auto", height: "270px" }} className='petPic' src={pics.large} alt="pet" /> })}</p>
             </div>
             <div className="noyes">
-              <div className="dislike" onClick={() => navigate(-1)}><img className="noyesIcon" src="/images/Avatars/dislike.png" width="120" /></div>
+              <div className="dislike" onClick={() => navigate(-1)}><img className="noyesIcon" src="/images/Avatars/goback.png" width="95px" /></div>
               <div className="like"><img className="noyesIcon" src="/images/Avatars/like.png" width="120" /></div>
             </div>
             <div className="PetProfileContent">
